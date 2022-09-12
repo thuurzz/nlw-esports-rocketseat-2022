5d70393d1166e7e252bcede5f945e924ecb5506b
Arthur Vinicius <thuur.vss@gmail.com>
2022-09-12 01:18:30 -0300

    criando app react native mobile

---
+++ esports/mobile/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Wold Mobile!</Text>
+      <Text>Este é um app criado com React Native!</Text>
+      <Button title="Enviar" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
