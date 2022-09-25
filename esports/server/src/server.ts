553e1978bc92c81b4f9057cb131a941e83807fc8
Arthur Vinicius <thuur.vss@gmail.com>
2022-09-25 17:41:06 -0300

    wip: web-0:34:48

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -2,14 +2,24 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (req, res) =>
-  res.status(200).json([
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  res.json([
     { id: 1, name: "Anúncio 1" },
     { id: 2, name: "Anúncio 2" },
     { id: 3, name: "Anúncio 3" },
     { id: 4, name: "Anúncio 4" },
     { id: 5, name: "Anúncio 5" },
-  ])
-);
+  ]);
+});
+
+app.get("/games/:id/discord", (req, res) => res.status(200).json([]));
 
 app.listen(3333);
